40f10716568bde16d52880652d44cbf45d8acfd2
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-15 18:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+for (let i = 1; i <= 20; i++) {
+    let output = "";
+
+    if (i % 3 === 0) {
+        output += "Fizz";
+    }
+
+    if (i % 5 === 0) {
+        output += "Buzz";
+    }
+
+    // If neither multiple of 3 nor 5, output the number itself
+    if (output === "") {
+        output = i;
+    }
+
+    // Display the result using alert
+    alert(output);
+}
